92bcb716a69dfb9f2b40a8d8c686723cdefd2be1
Lavish Sharma <sharmalavish375@gmail.com>
2024-05-11 00:18:17 +0530

    setting ejs as view engine

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.set('view engine','ejs');
+
 
 app.get("/",function(req,res)
 {
-  res.send("chal rha hai");
+  res.render("index");
 });
 
 app.listen(3000,function()
