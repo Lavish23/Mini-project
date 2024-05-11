535a5bef338156084a1c75eb69f16a9780d1e9f7
Lavish Sharma <sharmalavish375@gmail.com>
2024-05-11 15:02:02 +0530

    dynamic/routing

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require('express');
+const path=require('path');
 const app=express();
 
 //setting up the parsers
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+//setting up public static files
+app.use(express.static(path.join(__dirname,'public')));
+// console.log(path.join(__dirname+'/public')) 
+// console.log(__dirname+'/public');
 app.set('view engine','ejs');
 
 
@@ -13,7 +17,18 @@ app.get("/",function(req,res)
   res.render("index");
 });
 
+app.get("/author/:username",function(req,res)
+{
+  const temp=req.params.username;
+  res.send(`${temp}`);
+});
+
+app.get("/author/:username/:age",function(req,res)
+{
+  res.send(`Name: ${req.params.username}  Age: ${req.params.age}`)
+})
+
 app.listen(3000,function()
 {
-  console.log("It is running")
+  console.log("It is running....")
 });
